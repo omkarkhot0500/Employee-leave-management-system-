9ecb457677726eb6adf38360bbe8be6e498923d4
omkarkhot0500 <omkarkhot0500@gmail.com>
2025-07-14 17:22:01 +0530

    CORS update

--- server/server.js
+++ server/server.js
@@ -1,27 +1,41 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js';
-import leaveRoutes from './routes/leaveRoutes.js';
-import userRoutes from './routes/userRoutes.js';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import leaveRoutes from "./routes/leaveRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173", // dev
+  "https://gat-cse-aiml-lms.netlify.app", 
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/leave', leaveRoutes);
-app.use('/api/user', userRoutes);
-app.get('/', (req, res)=>{
-    res.send("Hello, This is the backend of HyScaler Employee leave management system.")
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/leave", leaveRoutes);
+app.use("/api/user", userRoutes);
+app.get("/", (req, res) => {
+  res.send(
+    "Hello, This is the backend of HyScaler Employee leave management system."
+  );
+});
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log('Mongo Error:', err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("Mongo Error:", err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

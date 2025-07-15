6ecfe5a37ebfbb0ae9f289735d6abd5bc46f5f34
omkarkhot0500 <omkarkhot0500@gmail.com>
2025-07-15 21:56:49 +0530

    clean-up

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -54,7 +54,6 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           {/* Logo Section */}
-          {/* Logo Section */}
           <div
             className={`transform transition-all duration-700 ${
               isVisible

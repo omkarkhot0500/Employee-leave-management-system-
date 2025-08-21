160730c691e62d4950db904c7b28fe0ab9031eac
omkarkhot0500 <omkarkhot0500@gmail.com>
2025-08-21 23:49:48 +0530

    cors update

--- server/server.js
+++ server/server.js
@@ -16,7 +16,13 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed for this origin"));
+      }
+    },
     credentials: true,
   })
 );
